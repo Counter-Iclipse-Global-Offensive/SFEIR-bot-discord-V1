71ad81d1e44b2825867f3d96eecda4683dbb905d
thisisnzed <0185thomas@gmail.com>
2022-12-02 05:36:11 +0100

    Update syphilisQuizz.js

--- events/syphilisQuizz.js
+++ events/syphilisQuizz.js
@@ -174,81 +174,84 @@ module.exports = {
                     Vous pouvez réassayer autant de fois que vous le souhaitez.
                 `)
                 .setColor("DarkRed")
-            if(interaction.customId === 'syphilis_one_first_option') {
-                await interaction.reply({ embeds: [embed2], components: [row2] });
-            } else if(interaction.customId === 'syphilis_one_second_option') {
-                await interaction.reply({ embeds: [embed2], components: [row2] });
-            } else if(interaction.customId === 'syphilis_one_third_option') {
-                memberModel.syphilisGoodAnswers++;
-                await memberModel.save();
-                await interaction.reply({ embeds: [embed2], components: [row2] });
-            } else if(interaction.customId === 'syphilis_one_fourth_option') {
-                await interaction.reply({ embeds: [embed2], components: [row2] });
-            } else if(interaction.customId === 'syphilis_two_first_option') {
-                await interaction.reply({ embeds: [embed3], components: [row3] });
-            } else if(interaction.customId === 'syphilis_two_second_option') {
-                await interaction.reply({ embeds: [embed3], components: [row3] });
-            } else if(interaction.customId === 'syphilis_two_third_option') {
-                await interaction.reply({ embeds: [embed3], components: [row3] });
-            } else if(interaction.customId === 'syphilis_two_fourth_option') {
-                memberModel.syphilisGoodAnswers++;
-                await memberModel.save();
-                await interaction.reply({ embeds: [embed3], components: [row3] });
-            } else if(interaction.customId === 'syphilis_three_first_option') {
-                await interaction.reply({ embeds: [embed4], components: [row4] });
-            } else if(interaction.customId === 'syphilis_three_second_option') {
-                await interaction.reply({ embeds: [embed4], components: [row4] });
-            } else if(interaction.customId === 'syphilis_three_third_option') {
-                memberModel.syphilisGoodAnswers++;
-                await memberModel.save();
-                await interaction.reply({ embeds: [embed4], components: [row4] });
-            } else if(interaction.customId === 'syphilis_three_fourth_option') {
-                await interaction.reply({ embeds: [embed4], components: [row4] });
-            } else if(interaction.customId === 'syphilis_four_first_option') {
-                await interaction.reply({ embeds: [embed5], components: [row5] });
-            } else if(interaction.customId === 'syphilis_four_second_option') {
-                memberModel.syphilisGoodAnswers++;
-                await memberModel.save();
-                await interaction.reply({ embeds: [embed5], components: [row5] });
-            } else if(interaction.customId === 'syphilis_four_third_option') {
-                await interaction.reply({ embeds: [embed5], components: [row5] });
-            } else if(interaction.customId === 'syphilis_four_fourth_option') {
-                await interaction.reply({ embeds: [embed5], components: [row5] });
-            } else if(interaction.customId === 'syphilis_five_first_option') {
-                if (memberModel.syphilisGoodAnswers === 4) {
-                    if(memberModel.syphilisBadge === false) {
-                        memberModel.syphilisBadge = true;
-                        await memberModel.save();
 
-                        const finalEmbedSuccess = new EmbedBuilder()
-                            .setTitle('Vous avez réussi !')
-                            .setDescription(`
-                                Vous avez eu 5 bonnes réponses, vous avez donc réussi !
-                                Vous êtes récompensés d'un badge, votre collection est visionnable sur le site.
-                            `)
-                            .setColor("DarkGreen")
 
-                        await interaction.reply({ embeds: [finalEmbedSuccess] });
-                    } else {
-                        const finalEmbedSuccess = new EmbedBuilder()
-                            .setTitle('Vous avez réussi !')
-                            .setDescription(`
-                                Vous avez eu 5 bonnes réponses, vous avez donc réussi !
-                                Vous avez déjà reçu le badge correspondant, votre collection est visionnable sur le site.
-                            `)
-                            .setColor("DarkGreen")
-
-                        await interaction.reply({ embeds: [finalEmbedSuccess] });
-                    }
-                } else {
-                    await interaction.reply({ embeds: [finalEmbedFail] });
-                }
-            } else if(interaction.customId === 'syphilis_five_second_option') {
-                await interaction.reply({ embeds: [finalEmbedFail] });
-            } else if(interaction.customId === 'syphilis_five_third_option') {
-                await interaction.reply({ embeds: [finalEmbedFail] });
-            } else if(interaction.customId === 'syphilis_five_fourth_option') {
-                await interaction.reply({ embeds: [finalEmbedFail] });
+            switch(interaction.customId) {
+            	case 'syphilis_one_first_option':
+            	case 'syphilis_one_second_option':
+            	case 'syphilis_one_fourth_option':
+                	await interaction.reply({ embeds: [embed2], components: [row2] });
+            		break;
+            	case 'syphilis_one_third_option':
+            		memberModel.syphilisGoodAnswers++;
+                	await memberModel.save();
+                	await interaction.reply({ embeds: [embed2], components: [row2] });
+              	 	break;
+            	case 'syphilis_two_first_option':
+            	case 'syphilis_two_second_option':
+            	case 'syphilis_two_third_option':
+                	await interaction.reply({ embeds: [embed3], components: [row3] });
+            		break;
+            	case 'syphilis_two_fourth_option':
+            		memberModel.syphilisGoodAnswers++;
+                	await memberModel.save();
+                	await interaction.reply({ embeds: [embed3], components: [row3] });
+              	 	break;
+            	case 'syphilis_three_first_option':
+            	case 'syphilis_three_second_option':
+            	case 'syphilis_three_fourth_option':
+                	await interaction.reply({ embeds: [embed4], components: [row4] });
+            		break;
+            	case 'syphilis_three_third_option':
+            		memberModel.syphilisGoodAnswers++;
+                	await memberModel.save();
+                	await interaction.reply({ embeds: [embed4], components: [row4] });
+              	 	break;
+            	case 'syphilis_four_first_option':
+            	case 'syphilis_four_third_option':
+            	case 'syphilis_four_fourth_option':
+                	await interaction.reply({ embeds: [embed5], components: [row5] });
+            		break;
+            	case 'syphilis_four_second_option':
+            		memberModel.syphilisGoodAnswers++;
+                	await memberModel.save();
+                	await interaction.reply({ embeds: [embed5], components: [row5] });
+              	 	break;
+            	case 'syphilis_five_second_option':
+            	case 'syphilis_five_third_option':
+            	case 'syphilis_five_fourth_option':
+                	await interaction.reply({ embeds: [finalEmbedFail] });
+            		break;
+            	case 'syphilis_five_first_option':
+					if (memberModel.syphilisGoodAnswers === 4) {
+                	    if(memberModel.syphilisBadge === false) {
+                	        memberModel.syphilisBadge = true;
+                	        await memberModel.save();
+	
+                	        const finalEmbedSuccess = new EmbedBuilder()
+                	            .setTitle('Vous avez réussi !')
+                	            .setDescription(`
+                	                Vous avez eu 5 bonnes réponses, vous avez donc réussi !
+                	                Vous êtes récompensés d'un badge, votre collection est visionnable sur le site.
+                	            `)
+                	            .setColor("DarkGreen")
+	
+                	        await interaction.reply({ embeds: [finalEmbedSuccess] });
+                	    } else {
+                	        const finalEmbedSuccess = new EmbedBuilder()
+                	            .setTitle('Vous avez réussi !')
+                	            .setDescription(`
+                	                Vous avez eu 5 bonnes réponses, vous avez donc réussi !
+                	                Vous avez déjà reçu le badge correspondant, votre collection est visionnable sur le site.
+                	            `)
+                	            .setColor("DarkGreen")
+	
+                	        await interaction.reply({ embeds: [finalEmbedSuccess] });
+                	    }
+                	} else {
+                	    await interaction.reply({ embeds: [finalEmbedFail] });
+                	}
+              	 	break;
             }
         }
     }
